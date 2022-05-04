301461eb235ba5f0615230a95f06937d4e075c19
Laura DUBUCHE <ldubuche@e1r9p13.42.fr>
2022-05-04 16:24:21 +0200

    correction abs de malloc

--- libft/srcs/ft_newstr.c
+++ libft/srcs/ft_newstr.c
@@ -3,20 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_newstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ldubuche <laura.dubuche@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 15:47:40 by nargouse          #+#    #+#             */
-/*   Updated: 2022/04/12 16:01:36 by nargouse         ###   ########.fr       */
+/*   Updated: 2022/05/04 15:23:09 by ldubuche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_newstr(size_t n)
 {
 	char	*result;
 
-	result = NULL;
+	result = (char *) malloc(sizeof(char) * n);
+	if (!result)
+		return (NULL);
 	ft_bzero(result, n);
 	return (result);
 }
