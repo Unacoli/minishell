995229672f8e8d33c382b304c414060cbd0cb149
ARGOUSE Nao <nargouse@student.42.fr>
2022-06-09 03:34:48 +0200

    test

--- srcs/ast/parse_command.c
+++ srcs/ast/parse_command.c
@@ -6,7 +6,7 @@
 /*   By: ldubuche <ldubuche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 22:34:42 by nargouse          #+#    #+#             */
-/*   Updated: 2022/06/09 02:20:49 by nargouse         ###   ########.fr       */
+/*   Updated: 2022/06/09 03:34:33 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,31 @@ return 1 if the node creation succeeded
 0 otherwise
 */
 
+static int	parse_pipe_seq(t_ast **ast, t_lexer *lexer)
+{
+	t_token	*token;
+
+	token = lexer->token[lexer->pos++];
+	while (token->type == TOKEN_PASS)
+		token = lexer->tokens[lexer->pos++];
+	if (token->type == TOKEN_NOT_VALID)
+		return (0);
+	else if (token->type == TOKEN_PIPE)
+	{
+		node->left = malloc_ast(NODE_PIPE_SEQ, );
+		node->right = malloc_ast(NODE_SIMPLE_CMD, );
+	}
+	return (1);
+}
+
 static int	pipe_seq(t_ast **ast, t_lexer *lexer)
 {
 	t_ast	*node;
 
-	lexer = NULL;
 	node = malloc_ast(node->type, node->data);
-	if (/*Function to parse pipe seq*/)
+	if (parse_pipe_seq(ast, lexer))
 	{
-		/*Function to take right node*/
+		simple_command(node->right, lexer);
 		return (1);
 	}
 	free_ast(node);
