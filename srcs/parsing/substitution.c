d4a47b5215d23b09edf54e3aa99a95679d4ac490
Laura DUBUCHE <ldubuche@e1r7p17.clusters.42paris.fr>
2022-07-14 15:33:56 +0200

    fix leak

--- srcs/parsing/substitution.c
+++ srcs/parsing/substitution.c
@@ -6,7 +6,7 @@
 /*   By: ldubuche <ldubuche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 23:19:52 by ldubuche          #+#    #+#             */
-/*   Updated: 2022/07/14 15:12:38 by ldubuche         ###   ########.fr       */
+/*   Updated: 2022/07/14 15:32:35 by ldubuche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,33 @@ char	*make_3_str(char *str, int position, int j, char *value)
 	ft_strlcpy(before_substi, str, position);
 	ft_strlcpy(substi, value, ft_strlen(value) + 1);
 	ft_strlcpy(after_substi, str + j, (ft_strlen(str) - j) + 1);
-	if (str[position] == '?')
-		free(value);
 	return (join_3_str(before_substi, substi, after_substi));
+}
+
+char	*set_value(char *str, int *vaue, int l, t_env *env)
+{
+	char	*variable;
+	char	*value;
+
+	variable = ft_calloc(l, sizeof(char));
+	ft_strlcpy(variable, str, l);
+	value = search_env(*env, variable);
+	free(variable);
+	if (value == NULL)
+		value = ft_strdup("");
+	else
+		*vaue = 1;
+	return (value);
 }
 
 char	*substitution(char *str, int position, t_env *env)
 {
 	int		j;
-	char	*variable;
 	char	*value;
+	int		vaue;
+	char	*temp;
 
+	vaue = 0;
 	j = position;
 	if (str[position] == '?')
 	{
@@ -55,14 +71,12 @@ char	*substitution(char *str, int position, t_env *env)
 		while (str[j] && (!is_space(str[j]) && (isalnum(str[j]) \
 		|| str[j] == '_')))
 			j++;
-		variable = ft_calloc(j - position + 1, sizeof(char));
-		ft_strlcpy(variable, str + position, (j - position + 1));
-		value = search_env(*env, variable);
-		free(variable);
-		if (value == NULL)
-			value = ft_strdup("");
+		value = set_value(str + position, &vaue, j - position + 1, env);
 	}
-	return (make_3_str(str, position, j, value));
+	temp = make_3_str(str, position, j, value);
+	if (!vaue)
+		free(value);
+	return (temp);
 }
 
 char	*search_substi(t_env *env, char *str)
