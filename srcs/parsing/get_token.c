13d75d43bcd29b4af5c8902fdec15563981ed509
Laura DUBUCHE <ldubuche@e1r7p17.clusters.42paris.fr>
2022-07-14 14:57:36 +0200

    fix substi dans token_word

--- srcs/parsing/get_token.c
+++ srcs/parsing/get_token.c
@@ -6,23 +6,27 @@
 /*   By: ldubuche <ldubuche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 17:38:59 by nargouse          #+#    #+#             */
-/*   Updated: 2022/07/12 19:21:58 by nargouse         ###   ########.fr       */
+/*   Updated: 2022/07/14 14:56:35 by ldubuche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_token	handle_word(char *input, t_token *rlist)
+static t_token	handle_word(char *input, t_token *rlist, t_env *env)
 {
 	int		i;
 	char	*str;
+	char	*temp;
 
 	i = 0;
 	i = delimite_word(input, i, rlist);
 	str = create_str(input, i);
 	if (str == NULL)
 		return ((t_token){NULL, 0, TOKEN_NOT_VALID});
-	return ((t_token){str, i, TOKEN_WORD});
+	temp = search_substi(env, str);
+	if (temp != str)
+		free(str);
+	return ((t_token){temp, i, TOKEN_WORD});
 }
 
 int	is_operator(char *input, t_token *rlist)
@@ -68,5 +72,5 @@ t_token	get_token(char *input, t_env *env, t_token *rlist)
 	if (*input == '$')
 		return (handle_substitution(input, env, rlist));
 	else
-		return (handle_word(input, rlist));
+		return (handle_word(input, rlist, env));
 }
